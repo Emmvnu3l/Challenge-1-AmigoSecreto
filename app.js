41048f4f7ba5a90347043d45c95fd5c412e6f3cc
Emmvnu3l <168144728+Emmvnu3l@users.noreply.github.com>
2025-08-05 10:56:32 -0400

    actualizacion de js

--- app.js
+++ app.js
@@ -3,15 +3,18 @@
 
 // Array para almacenar nombres
 let nombres = []
-let valor = document.getElementById("amigo").value
+let valor = document.getElementById("amigo")
 
 
 // funcion que obtiene amigos y lo agrega a la array
 function agregarAmigo() {
-    if (valor == ''){
+    if (valor.value == ''){
         alert('ingresa un valor valido')
     }else{
     return nombres.push(valor)}
 }
 
+    for (let i = 0; i < nombres.length; i ++){
+
+    }
 
