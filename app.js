f840fa9dfd980057cb6c7ac693d668c9f8275952
Emmvnu3l <168144728+Emmvnu3l@users.noreply.github.com>
2025-08-05 10:29:03 -0400

    actualizacion  de js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+// Array para almacenar nombres
+let nombres = []
+let valor = document.getElementById("nombre").value
+
+
+// funcion que obtiene amigos y lo agrega a la array
+function agregarAmigos() {
+    if (valor == ''){
+        alert('ingresa un valor valido')
+    }else{
+    return nombres.push(valor)}
+}
+
+
